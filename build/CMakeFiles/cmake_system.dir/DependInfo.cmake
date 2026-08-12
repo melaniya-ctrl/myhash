87d36ccdd499d82cb4979384f7cd89cbdd3444e4
melaniya-ctrl <nikonorova.melaniya2004@gmail.com>
2026-08-12 15:35:47 +0300

    Папка build с CMake сборкой

---
+++ build/CMakeFiles/cmake_system.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/nikon/mila/callbacks.c" "CMakeFiles/cmake_system.dir/callbacks.c.obj" "gcc" "CMakeFiles/cmake_system.dir/callbacks.c.obj.d"
+  "C:/Users/nikon/mila/db.c" "CMakeFiles/cmake_system.dir/db.c.obj" "gcc" "CMakeFiles/cmake_system.dir/db.c.obj.d"
+  "C:/Users/nikon/mila/main.c" "CMakeFiles/cmake_system.dir/main.c.obj" "gcc" "CMakeFiles/cmake_system.dir/main.c.obj.d"
+  "C:/Users/nikon/mila/salt.c" "CMakeFiles/cmake_system.dir/salt.c.obj" "gcc" "CMakeFiles/cmake_system.dir/salt.c.obj.d"
+  "C:/Users/nikon/mila/ui.c" "CMakeFiles/cmake_system.dir/ui.c.obj" "gcc" "CMakeFiles/cmake_system.dir/ui.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cmake_system.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_system.dir/callbacks.c.obj"
+  "CMakeFiles/cmake_system.dir/callbacks.c.obj.d"
+  "CMakeFiles/cmake_system.dir/db.c.obj"
+  "CMakeFiles/cmake_system.dir/db.c.obj.d"
+  "CMakeFiles/cmake_system.dir/main.c.obj"
+  "CMakeFiles/cmake_system.dir/main.c.obj.d"
+  "CMakeFiles/cmake_system.dir/salt.c.obj"
+  "CMakeFiles/cmake_system.dir/salt.c.obj.d"
+  "CMakeFiles/cmake_system.dir/ui.c.obj"
+  "CMakeFiles/cmake_system.dir/ui.c.obj.d"
+  "cmake_system.exe"
+  "cmake_system.exe.manifest"
+  "cmake_system.pdb"
+  "libcmake_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
